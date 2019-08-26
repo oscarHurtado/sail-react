93c35bd945a513daed37227bc2eface97f6b75cb
oscar <oehurtadoc@viajesfalabella.cl>
2019-08-26 11:20:36 -0400

    first commit

---
+++ assets/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const App = () => {
+  return (
+    <h1>Sample React and Sails ..</h1>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
